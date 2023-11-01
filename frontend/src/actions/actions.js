3c76035204c5ba3e2cdda4a9febb5f5ab9c8af9d
jayajput30 <rajputjay901@gmail.com>
2023-11-01 15:48:40 +0530

    feat: added reducer action creator functions

---
+++ frontend/src/actions/actions.js
@@ -0,0 +1,117 @@
+export const addNewIncome = (incomeData) => async (dispatch) => {
+  try {
+    dispatch({ type: "DATA_LOADING" });
+    const response = await fetch("http://localhost:3002/income", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(incomeData),
+    });
+    const data = await response.json();
+    if (data) {
+      dispatch({ type: "ADD_INCOME_SUCCESS", payload: data.data });
+    }
+  } catch (e) {
+    console.error(e.message);
+    dispatch({ type: "ADD_INCOME_FAILURE", payload: e.message });
+  } finally {
+    dispatch({ type: "DATA_LOADED" });
+  }
+};
+
+export const getAllIcome = () => async (dispatch) => {
+  try {
+    dispatch({ type: "DATA_LOADING" });
+    const response = await fetch("http://localhost:3002/income");
+    const data = await response.json();
+    if (data) {
+      dispatch({ type: "GET_INCOME_SUCCESS", payload: data.data });
+    }
+  } catch (e) {
+    console.error(e.message);
+    dispatch({ type: "GET_INCOME_FAILURE", payload: e.message });
+  } finally {
+    dispatch({ type: "DATA_LOADED" });
+  }
+};
+
+//expense
+
+export const addnewExpense = (expenseData) => async (dispatch) => {
+  try {
+    dispatch({ type: "DATA_LOADING" });
+    const response = await fetch("http://localhost:3002/income", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(expenseData),
+    });
+    const data = await response.json();
+    if (data) {
+      dispatch({ type: "ADD_EXPENSE_SUCCESS", payload: data.data });
+    }
+  } catch (e) {
+    console.error(e.message);
+    dispatch({ type: "ADD_EXPENSE_FAILURE", payload: e.message });
+  } finally {
+    dispatch({ type: "DATA_LOADED" });
+  }
+};
+
+export const getAllExpense = () => async (dispatch) => {
+  try {
+    dispatch({ type: "DATA_LOADING" });
+    const response = await fetch("http://localhost:3002/expense");
+    const data = await response.json();
+    if (data) {
+      dispatch({ type: "GET_EXPENSE_SUCCESS", payload: data.data });
+    }
+  } catch (e) {
+    console.error(e.message);
+    dispatch({ type: "GET_EXPENSE_FAILURE", payload: e.message });
+  } finally {
+    dispatch({ type: "DATA_LOADED" });
+  }
+};
+
+//saving
+
+export const addewIncome = (savingData) => async (dispatch) => {
+  try {
+    dispatch({ type: "DATA_LOADING" });
+    const response = await fetch("http://localhost:3002/saving", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(savingData),
+    });
+    const data = await response.json();
+    if (data) {
+      dispatch({ type: "ADD_SAVING_SUCCESS", payload: data.data });
+    }
+  } catch (e) {
+    console.error(e.message);
+    dispatch({ type: "ADD_SAVING_FAILURE", payload: e.message });
+  } finally {
+    dispatch({ type: "DATA_LOADED" });
+  }
+};
+
+export const getAllSaving = () => async (dispatch) => {
+  try {
+    dispatch({ type: "DATA_LOADING" });
+    const response = await fetch("http://localhost:3002/saving");
+    const data = await response.json();
+    if (data) {
+      dispatch({ type: "GET_SAVING_SUCCESS", payload: data.data });
+    }
+  } catch (e) {
+    console.error(e.message);
+    dispatch({ type: "GET_SAVING_FAILURE", payload: e.message });
+  } finally {
+    dispatch({ type: "DATA_LOADED" });
+  }
+};
--- frontend/src/reducers/financeReducer.js
+++ frontend/src/reducers/financeReducer.js
@@ -11,22 +11,55 @@ export const financeReducer = (state = initialState, action) => {
     case "DATA_LOADING": {
       return { ...state, loading: true };
     }
+    case "DATA_LOADED": {
+      return { ...state, loading: false };
+    }
     case "ADD_INCOME_SUCCESS": {
-      return { ...state, loading: false, income: action.payload };
+      return {
+        ...state,
+        loading: false,
+        income: [...state.income, action.payload],
+      };
     }
     case "ADD_INCOME_FAILURE": {
       return { ...state, loading: false, error: action.payload };
     }
+    case "GET_INCOME_SUCCESS": {
+      return { ...state, loading: false, income: action.payload };
+    }
+    case "GET_INCOME_FAILURE": {
+      return { ...state, loading: false, error: action.payload };
+    }
     case "ADD_EXPENSE_SUCCESS": {
-      return { ...state, loading: false, expense: action.payload };
+      return {
+        ...state,
+        loading: false,
+        expense: [...state.expense, action.payload],
+      };
     }
     case "ADD_EXPENSE_FAILURE": {
       return { ...state, loading: false, error: action.payload };
     }
+    case "GET_EXPENSE_SUCCESS": {
+      return { ...state, loading: false, expense: action.payload };
+    }
+    case "GET_EXPENSE_FAILURE": {
+      return { ...state, loading: false, error: action.payload };
+    }
     case "ADD_SAVING_SUCCESS": {
-      return { ...state, loading: false, saving: action.payload };
+      return {
+        ...state,
+        loading: false,
+        saving: [...state.saving, action.payload],
+      };
     }
     case "ADD_SAVING_FAILURE": {
+      return { ...state, loading: false, error: action.payload };
+    }
+    case "GET_SAVING_SUCCESS": {
+      return { ...state, loading: false, saving: action.payload };
+    }
+    case "GET_SAVING_FAILURE": {
       return { ...state, loading: false, error: action.payload };
     }
     default:
